7b2aca1b73e84a5ed183d2836b415ff5907645c9
mathewsmotta <mathewscsmotta@gmail.com>
2020-10-04 16:37:09 +0100

    Added config for data package

---
+++ gre-todo-data/src/main/java/com/goldenrealstate/gretodo/data/IDataPackage.java
@@ -0,0 +1,11 @@
+package com.goldenrealstate.gretodo.data;
+
+/**
+ * This interface is used for dependency injection configuration purposes.
+ * A preferable way different than strings so it's refactor-friendly
+ *
+ * @since 1.0
+ * @author Mathews Motta
+ */
+public interface IDataPackage {
+}
---
+++ gre-todo-data/src/main/java/com/goldenrealstate/gretodo/data/config/DataConfig.java
@@ -0,0 +1,22 @@
+package com.goldenrealstate.gretodo.data.config;
+
+import com.goldenrealstate.gretodo.data.IDataPackage;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Configures the data module by package class and not strings
+ *
+ * @since 1.0
+ * @author Mathews Motta
+ */
+@Configuration
+@ComponentScan(basePackageClasses = {IDataPackage.class})
+@EntityScan(basePackageClasses = {IDataPackage.class})
+@EnableJpaRepositories(basePackageClasses = {IDataPackage.class})
+@EnableTransactionManagement
+public class DataConfig {
+}
